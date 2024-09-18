181150148c32adc88f4964ada0e22c464a2bb987
abbeyperini <abbeyperini@gmail.com>
2024-09-18 19:21:49 -0400

    Baby's first angular component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ContentProjection } from './content-projection.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ContentProjection],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/content-projection.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  standalone: true,
+  selector: "content-projection",
+  template: `<div><p>It me</p></div>`
+})
+export class ContentProjection { }
