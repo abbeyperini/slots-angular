e30ebdc2b8d47c0b459ca5e895f57ac96f0da431
abbeyperini <abbeyperini@gmail.com>
2024-09-21 13:35:13 -0400

    styling

--- src/app/components/default-content-projection.component.ts
+++ src/app/components/default-content-projection.component.ts
@@ -3,10 +3,10 @@ import { Component } from "@angular/core";
 @Component({
   standalone: true,
   selector: "default-content-projection",
-  template: `<div class="the-club">
-    <p>The Window</p>
-    <ng-content>Default</ng-content>
-    <p>The Wall</p>
+  template: `<div class="club-container">
+      <img src="/the-window.jpg" alt="the window" />
+      <ng-content><img src="confused-lil-jon.jpg" alt="lil jon saying 'what?!'" /></ng-content>
+      <img src="/the-wall.png" alt="the wall" />
     </div>`
 })
 export class DefaultContentProjection { }
