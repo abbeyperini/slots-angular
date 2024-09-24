1a325b33588bb6f0dd8afe3793ebffc838153995
abbeyperini <abbeyperini@gmail.com>
2024-09-24 13:19:01 -0400

    Whitespace, commenting

--- src/app/components/named-content-projection.component.ts
+++ src/app/components/named-content-projection.component.ts
@@ -4,11 +4,9 @@ import { Component } from "@angular/core";
   standalone: true,
   selector: "named-content-projection",
   template: `<div class="club-container">
-    <ng-content select=".the-window"><p>The Window</p></ng-content>
-    <!-- <ng-content select="the-window"><p>The Window</p></ng-content> -->
-    <ng-content><img src="confused-lil-jon.jpg" alt="lil jon saying 'what?!'" /></ng-content>
-    <ng-content select=".the-wall"><p>The Wall</p></ng-content>
-    <!-- <ng-content select="the-wall"><p>The Wall</p></ng-content> -->
+      <ng-content select=".the-window"><p>The Window</p></ng-content>
+      <ng-content><img src="confused-lil-jon.jpg" alt="lil jon saying 'what?!'" /></ng-content>
+      <ng-content select=".the-wall"><p>The Wall</p></ng-content>
     </div>`
 })
 export class NamedContentProjection { }
