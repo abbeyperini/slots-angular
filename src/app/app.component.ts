7fbccff68f727fdcbf70ba9c5b6478e973accfb9
abbeyperini <abbeyperini@gmail.com>
2025-09-16 14:58:39 -0400

    Update to v19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,10 @@ import { RouterOutlet } from '@angular/router';
 import { DefaultContentProjection } from './components/default-content-projection.component';
 import { NamedContentProjection } from './components/named-content-projection.component';
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, DefaultContentProjection, NamedContentProjection],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    imports: [RouterOutlet, DefaultContentProjection, NamedContentProjection],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'slots';
