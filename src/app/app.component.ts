1865bc0c840966e40a022fa5ed4b4142a061c82d
abbeyperini <abbeyperini@gmail.com>
2025-09-17 16:07:06 -0400

    Add conditional content projection examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DefaultContentProjection } from './components/default-content-projection.component';
 import { NamedContentProjection, TheWall, TheWindow } from './components/named-content-projection.component';
+import { ConditionalContentProjection } from './components/conditional-content-projection.component';
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, DefaultContentProjection, NamedContentProjection, TheWall, TheWindow],
+    imports: [RouterOutlet, DefaultContentProjection, NamedContentProjection, TheWall, TheWindow, ConditionalContentProjection],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
---
+++ src/app/components/conditional-content-projection.component.ts
@@ -0,0 +1,42 @@
+import { Component, inject, input, viewChild, TemplateRef, ViewContainerRef } from "@angular/core";
+import { NgTemplateOutlet } from "@angular/common";
+
+@Component({
+  selector: "in-the-club",
+  template: `
+    <ng-template #lilJon><img src="confused-lil-jon.jpg" alt="lil jon saying 'what?!'" /></ng-template>
+    <ng-template #peopleInTheClub [let-people]="people" ><img [src]="people()" alt="people in the club" /></ng-template>
+  `
+})
+export class InTheClub {
+  private viewContainer = inject(ViewContainerRef);
+  people = input<string>();
+  showPeople = input<boolean>(false);
+  lilJon = viewChild<TemplateRef<unknown>>("lilJon");
+  peopleInTheClub = viewChild<TemplateRef<unknown | undefined>>("peopleInTheClub");
+  ngOnInit() {
+    if (this.showPeople()) {
+      this.viewContainer.createEmbeddedView(this.peopleInTheClub()!, {people: this.people });
+    } else {
+      this.viewContainer.createEmbeddedView(this.lilJon()!);
+    }
+  }
+}
+@Component({
+  selector: "conditional-content-projection",
+  imports: [NgTemplateOutlet, InTheClub],
+  template: `<div class="club-container">
+    <img src="/the-window.jpg" alt="the window" />
+    <ng-template #lilJon><img src="confused-lil-jon.jpg" alt="lil jon saying 'what?!'" /></ng-template>
+    <!-- <ng-template #peopleInTheClub [let-people]="people" ><img [src]="people()" alt="people in the club" /></ng-template> -->
+    <ng-container *ngTemplateOutlet="lilJon"></ng-container>
+    <!-- <ng-container [ngTemplateOutlet]="peopleInTheClub" [ngTemplateOutletContext]="people" class="club-container"></ng-container> -->
+    <!-- <in-the-club [showPeople]="false" people="/club-people.jpeg"></in-the-club>
+    <img src="/the-wall.png" alt="the wall" /> -->
+  </div>`
+})
+export class ConditionalContentProjection {
+  people = input<string | undefined>();
+  lilJon = viewChild<TemplateRef<unknown | undefined>>("lilJon");
+  peopleInTheClub = viewChild<TemplateRef<unknown | undefined>>("peopleInTheClub");
+}
